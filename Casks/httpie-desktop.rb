38e79d0a1f84bd8281d642f87c11b3ccab58e81c
daffyDukk <daniel.p.forsberg@gmail.com>
2022-12-14 16:28:20 +0100

    Added new cask HTTPie Desktop

---
+++ Casks/httpie-desktop.rb
@@ -0,0 +1,17 @@
+cask "httpie-desktop" do
+  arch arm: "-arm64", intel: ""
+
+  version "2022.16.0"
+  sha256 arm:   "a557901161e35a0a9f6373b383b87043f6fa541009bf0ae5297314cc01b0961a",
+         intel: "2af6bc5f8b2895ce03dd63f159541d121d88ab8b9696c1a48c783e48e98999fd"
+
+  url "https://github.com/httpie/desktop/releases/download/v#{version}/HTTPie-#{version}#{arch}.dmg",
+      verified: "github.com/httpie/desktop"
+
+  name "HTTPie Desktop"
+  desc "HTTPie Desktop interface"
+  homepage "https://httpie.io/"
+
+
+  app "HTTPie.app"
+end
